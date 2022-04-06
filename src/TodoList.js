32bd23274e0cdb8933559c19d5fedef22c3d2f04
eldomador <johncenajakub@gmail.com>
2022-04-06 14:02:55 +0200

    feat: example HOC react app

---
+++ src/TodoList.js
@@ -0,0 +1,22 @@
+import HOC from "./HOC";
+
+const TodoList = ({ data }) => {
+  let renderTodos = data.map((todo) => {
+    return (
+      <div key={todo.userId}>
+        <p>
+          <strong>{todo.title}</strong>
+        </p>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div>{renderTodos}</div>
+    </div>
+  );
+};
+
+const SearchTodos = HOC(TodoList, "todos");
+
+export default SearchTodos;
